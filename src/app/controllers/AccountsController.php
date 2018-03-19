666b002a0329b2a59d9da9fe5fcee88f98a3f5a4
Sam Ringleman <sam.ringleman@gmail.com>
2018-03-19 17:14:12 -0600

    add accounts controller

---
+++ src/app/controllers/AccountsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace controllers;
+
+class AccountsController extends Controller
+{
+
+    public function index()
+    {
+        echo 'you have made it to the accounts controller';
+    }
+
+}
