da07ba5ccfe7d61e7e6298a42b4ea01951d97c70
Sam Ringleman <sam.ringleman@gmail.com>
2018-03-22 12:37:07 -0600

    configure account validation and filter rules, also add tableName method

--- src/app/models/Account.php
+++ src/app/models/Account.php
@@ -4,5 +4,35 @@ namespace models;
 
 class Account extends Model
 {
+    public $attributes = [
+        'id',
+        'accountnumber',
+        'type',
+        'amount',
+        'created'
+    ];
 
+    public $validationRules = [
+        'created' => 'date',
+        'accountnumber' => 'required|integer',
+        'type' => 'required|alpha',
+        'amount' => 'required|numeric'
+    ];
+
+    public $filterRules = [
+        'created' => 'trim|sanitize_numbers',
+        'acountnumber' => 'trim|sanitize_numbers',
+        'type' => 'trim|sanitize_string',
+        'amount' => 'trim|sanitize_floats'
+    ];
+
+    public function __construct($db)
+    {
+        parent::__construct($db, self::tableName());
+    }
+
+    public static function tableName()
+    {
+        return 'Accounts';
+    }
 }
