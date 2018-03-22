e60a17cd85533dbb90943169d7c39ffcb79b0ee6
Sam Ringleman <sam.ringleman@gmail.com>
2018-03-22 06:01:18 -0600

    add guzzle and start setting up transaction properties

--- src/app/models/Transaction.php
+++ src/app/models/Transaction.php
@@ -5,4 +5,18 @@ namespace models;
 class Transaction extends Model
 {
 
+    public $attributes = [
+        'id',
+        'logged',
+        'date',
+        'storeid',
+        'accountid',
+        'type',
+        'amount'
+    ];
+
+    public $validationRules = [];
+
+    public $filterRules = [];
+
 }
