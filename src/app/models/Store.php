dd5d952d5cf6e8b40c6de1956a409d56b857cbef
Sam Ringleman <sam.ringleman@gmail.com>
2018-03-22 12:41:01 -0600

    set vallidation and filter rules for stores

--- src/app/models/Store.php
+++ src/app/models/Store.php
@@ -4,5 +4,26 @@ namespace models;
 
 class Store extends Model
 {
+    public $attributes = [
+        'id',
+        'name'
+    ];
 
+    public $validationRules = [
+        'name' => 'required|alpha',
+    ];
+
+    public $filterRules = [
+        'name' => 'trim|sanitize_string',
+    ];
+
+    public function __construct($db)
+    {
+        parent::__construct($db, self::tableName());
+    }
+
+    public static function tableName()
+    {
+        return 'Stores';
+    }
 }
