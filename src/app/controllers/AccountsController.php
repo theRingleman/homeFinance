d8e3dc60679cf66127bb8074338b078ed5b29c37
Sam Ringleman <sam.ringleman@gmail.com>
2018-03-23 15:56:04 -0600

    print out errors when account is not saved

--- src/app/controllers/AccountsController.php
+++ src/app/controllers/AccountsController.php
@@ -16,7 +16,17 @@ class AccountsController extends Controller
     {
         $account = new Account($this->db);
         if (!empty($this->attributes)) {
-            $account->create($this->attributes);
+            if ($account->create($this->attributes)) {
+                $this->renderJson([
+                    'message' => 'Account created successfully',
+                    'account' => $account->toEndPoint()
+                ]);
+            } else {
+                $this->renderJson([
+                    "message" => "Something went horribly wrong",
+                    "errors" => $account->errors
+                ]);
+            }
         } else {
             throw new \Exception("You are missing the required parameters", 401);
         }
